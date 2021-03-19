d144ca2e4f70d4120a2e7abe857c1a2523728ba0
pi <mwilson@vocera.com>
2021-03-19 10:55:29 -0400

    Improved Logging, start build-out of sensors, etc

--- project1/LineGame.py
+++ project1/LineGame.py
@@ -5,15 +5,40 @@ import RPi.GPIO as mygpio
 from datetime import datetime
 import logging as sessionLog
 
-sessionLog.basicConfig(filename=datetime.now().strftime('LineGame_%H_%M_%d_%m_%Y.log'), level=sessionLog.INFO) #Basic debug logging
+myLeftLineSensor = mygpio
+myRightLineSensor = mygpio
+
+sessionLogFormat = "ROBITLOG:" + "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+sessionLog.basicConfig(filename=datetime.now().strftime('LineGame_%H_%M_%d_%m_%Y.log'), level=sessionLog.INFO,format=sessionLogFormat) #Basic debug logging
+
+#Robit wheel speed
 LEFT_TRIM   = 0
 RIGHT_TRIM  = 0
 TURN_SPEED = 50
-NORM_SPEED = 150
-SLOW_SPEED = 75
+NORM_SPEED = 100
+SLOW_SPEED = 50
+
+# Line sensors
+_LEFTSENSORGPIOVALUE = 7
+_RIGHTSENSORGPIOVALUE = 18
+myLeftLineSensor.setwarnings(False)
+myLeftLineSensor.setmode(mygpio.BOARD)
+myLeftLineSensor.setup(_LEFTSENSORGPIOVALUE ,mygpio.IN) # maps to 4 on HAT
+myRightLineSensor.setwarnings(False)
+myRightLineSensor.setmode(mygpio.BOARD)
+myRightLineSensor.setup(_RIGHTSENSORGPIOVALUE,mygpio.IN) # maps to 24 on HAT
+
+
 sessionLog.info("****** DEFAULT VALUES ******")
+sessionLog.info("MOTOR CONFIG")
+sessionLog.info("------------")
 sessionLog.info('LEFT TRIM = '+ str(LEFT_TRIM) + '     RIGHT TRIM = ' + str(RIGHT_TRIM)) #record starting trim levels in log
 sessionLog.info ('TURN SPEED = '+ str(TURN_SPEED) + '     NORMAL SPEED = ' + str(NORM_SPEED) + '     SLOW SPEED = ' + str(SLOW_SPEED))
+sessionLog.info("SENSOR CONFIG")
+sessionLog.info("-------------")
+sessionLog.info ("LEFT SENSOR IS MAPPED TO PIN: "+ str(_LEFTSENSORGPIOVALUE))
+sessionLog.info ("RIGHT SENSOR IS MAPPED TO PIN : "+ str(_RIGHTSENSORGPIOVALUE))
+
 sessionLog.info("")
 myRobot = Robot.Robot(left_trim=LEFT_TRIM,right_trim=RIGHT_TRIM,left_id=1,right_id=4)
 
@@ -23,19 +48,19 @@ class LineGame:
 def eBrake():
   
     myRobot.stop()
-def doInit(): #tests wheels/calibration
+def InitTest(): #tests wheels/calibration
     sessionLog.debug('doInit')
-    myRobot.forward(100,.50)
-    myRobot.backward(100,.5)
-    myRobot.left(100,.5)
-    myRobot.right(100,.5)
+    myRobot.forward(50,.50)
+    myRobot.backward(50,.5)
+    myRobot.left(50,.5)
+    myRobot.right(50,.5)
 def checkSensorLeft():
     sensorleftvalue = 0
     print("")
 def checkSensorRight():
     sensorrightvalue = 0
     print("")
-def SensorDriveValue(): 
+def sensorDriveValue(): 
     drivevalue = 0 #default return value to stop for safety
     """This checks the sensors first left then right.
 
@@ -46,11 +71,15 @@ def SensorDriveValue():
         2 = RIGHT"""
 
     return drivevalue
-def start_game():
-    """ Starts the game."""
+def start_game(doTest):
+    """ Starts the game. If doTest = true an inital motor test will occur"""
     sessionLog.info('start_game - starting game')
-    doInit()
+    if (doTest == True): 
+        sessionLog.info("Doing preliminary testing")
+        InitTest()
     print("Starting Line Game")
-    sessionLog.info('SensorDriveValue: '+ str(SensorDriveValue()))
-   
-start_game()
+    sessionLog.info('SensorDriveValue: '+ str(sensorDriveValue()))
+    gameLooping = True
+    while gameLooping is True:
+        sensorDriveValue
+start_game(True)
